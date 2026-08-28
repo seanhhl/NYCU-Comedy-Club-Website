decbd334d4836f8b6fd8e675a337101574dff49b
seanhhl <sean.hanhsiang@gmail.com>
2026-08-28 15:24:39 +0800

    Update site URL; change brand name and adjust favicon link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://seanhhl.github.io',
-  base: '/NYCU-Comedy-Club-Website',
+  site: 'http://sean-club.nycu.cc',
 });
